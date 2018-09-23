085bf75bb21ebd58956243e0efb225c16da950ae
duongsang97 <39148557+duongsang97@users.noreply.github.com>
2018-09-23 17:05:14 +0700

    Cập nhật đăng nhập

--- BaiGuiXe_Smart_API/Areas/QuanTriVien/Controllers/HomeController.cs
+++ BaiGuiXe_Smart_API/Areas/QuanTriVien/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BaiGuiXe_Smart_API.Models;
+using BaiGuiXe_Smart_API.Models.UserSession;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,60 @@ namespace BaiGuiXe_Smart_API.Areas.QuanTriVien.Controllers
     public class HomeController : Controller
     {
         // GET: QuanTriVien/Home
+        UserModel use_Mod = new UserModel();
+        // GET: QuanTriVien/Home
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(FormCollection f)
+        {
+            if (f["txt_use"] != null && f["txt_pass"] != null)
+            {
+                MaHoa mh = new MaHoa();
+                var pass = mh.md5(f["txt_pass"]);
+                var flag = use_Mod.login(f["txt_use"], pass);
+                if (flag != null)
+                {
+                    if (flag.LoaiTaiKhoan == 1)
+                    {
+                        var usesession = new UserSession();
+                        usesession.Id = flag.Id.ToString();
+                        usesession.Ten = flag.Ten;
+                        usesession.Email = flag.Email;
+                        usesession.LoaiTaiKhoan = flag.LoaiTaiKhoan;
+                        usesession.XacThucEmail = flag.XacThucEmail;
+                        SessionHelper.SetSession(usesession);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Tài khoản của bạn không có quyền truy cập !");
+                        return View();
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Tài khoản và mật khẩu không chính xác !");
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tài khoản và mật khẩu không được bỏ trống !");
+                return View();
+            }
+
+
+        }
     }
-}
+} 
+    
