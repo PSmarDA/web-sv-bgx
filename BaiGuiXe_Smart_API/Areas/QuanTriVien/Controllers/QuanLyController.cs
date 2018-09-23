9894e2ab4fb1d9fb02b543b9c30c8d5dfd05acd9
Dzu Tran <34890975+dzu2801@users.noreply.github.com>
2018-09-23 17:47:40 +0700

    update baixe

--- BaiGuiXe_Smart_API/Areas/QuanTriVien/Controllers/QuanLyController.cs
+++ BaiGuiXe_Smart_API/Areas/QuanTriVien/Controllers/QuanLyController.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BaiGuiXe_Smart_API.Models.BaiXe;
 
 namespace BaiGuiXe_Smart_API.Areas.QuanTriVien.Controllers
 {
     public class QuanLyController : Controller
     {
+        BaiXe_Model baixe_model = new BaiXe_Model();
         // GET: QuanTriVien/QuanLy
         public ActionResult Index()
         {
+
             return View();
         }
 
         public ActionResult QLBaiXe()
         {
-            return View();
+            var bxlist = baixe_model.FindAll();
+
+            return View(bxlist);
         }
     }
 }
