5de773580b75d55b212ade5dbf3ce2b6977648ef
Dzu Tran <34890975+dzu2801@users.noreply.github.com>
2018-09-23 16:45:31 +0700

    Update BaiXe

---
+++ BaiGuiXe_Smart_API/Models/BaiXe/BaiXe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BaiGuiXe_Smart_API.Models.BaiXe
+{
+    public class BaiXe
+    {
+        [BsonId]
+        public ObjectId id
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [BsonElement("DiaChi")]
+        public string DiaChi
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [BsonElement("SucChua")]
+        public int SucChua
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [BsonElement("ChuSoHuu")]
+        public ObjectId ChuSoHuu
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ BaiGuiXe_Smart_API/Models/BaiXe/BaiXe_Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace BaiGuiXe_Smart_API.Models.BaiXe
+{
+    public class BaiXe_Model
+    {
+        Connect_MongoDB<BaiXe> db;
+        public BaiXe_Model()
+        {
+            db = new Connect_MongoDB<BaiXe>("BaiXe");
+        }
+
+        public void Create(BaiXe bx)
+        {
+            db.mongocollection.InsertOne(bx);
+        }
+
+        //Update collection
+        public void Update(BaiXe bx)
+        {
+            db.mongocollection.UpdateOne(
+                Builders<BaiXe>.Filter.Eq("_id", bx.Id),
+                Builders<BaiXe>.Update
+                .Set("DiaChi", bx.DiaChi)
+                .Set("SucChua", bx.SucChua)
+                .Set("ChuSoHuu", bx.ChuSoHuu)
+                );
+        }
+
+        //Xoa
+        public void Delete(ObjectId id)
+        {
+            db.mongocollection.DeleteOne(
+                Builders<BaiXe>.Filter.Eq("id", id)
+                );
+        }
+    }
+}
