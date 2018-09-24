a354fed5a091c0b2c8362b67e5b068495b4f9bd1
Dzu Tran <34890975+dzu2801@users.noreply.github.com>
2018-09-24 23:23:52 +0700

    update model

---
+++ BaiGuiXe_Smart_API/Models/GiaoDich/GiaoDich_Model.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiGuiXe_Smart_API.Models.GiaoDich
+{
+    public class GiaoDich_Model
+    {
+        Connect_MongoDB<GiaoDich> db;
+        public GiaoDich_Model()
+        {
+            db = new Connect_MongoDB<GiaoDich>("GiaoDich");
+        }
+
+        public List<GiaoDich> FindAll()
+        {
+            return db.mongocollection.AsQueryable().ToList();
+        }
+
+
+    }
+}
